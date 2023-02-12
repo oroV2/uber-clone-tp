0dedfb69e03cd5dff8da7bf626de2bbc041ef67f
oroV2 <onkarpawar58@gmail.com>
2023-02-12 18:14:34 +0530

    changes

--- App.js
+++ App.js
@@ -1,12 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import { store } from './store';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider i react-redux store = {store}>
+      <SafeAreaProvider>
+      <HomeScreen />
+      </SafeAreaProvider>
+    </Provider> 
   );
 }
 
@@ -18,3 +23,4 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
