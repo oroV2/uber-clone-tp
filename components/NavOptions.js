ea1260a511261a7281e3c46c3c502dc215527d3c
oroV2 <onkarpawar58@gmail.com>
2023-02-17 21:38:58 +0530

    change

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -1,7 +1,8 @@
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native'
 import React from 'react'
 import tw from 'tailwind-react-native-classnames';
-import { Icon } from 'react-native-vector-icons/Icon';
+import { Icon } from 'react-native-elements';
+import { useNavigation } from '@react-navigation/native';
 
 const data = [
     {
@@ -19,13 +20,17 @@ const data = [
 ];
 
 const NavOptions = () => {
+    const navigation = useNavigation();
+
   return (
     <FlatList
         data = {data}
         horizontal
         keyExtractor={(item) => item.id}
         renderItem={({item}) => (
-            <TouchableOpacity style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
+            <TouchableOpacity 
+            onPress={() => navigation.navigate(item.screen) }
+            style={tw `p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}>
                 <View>
                     <Image
                     style={{ width: 120, height:120, resizeMode: "contain"}}
@@ -36,7 +41,10 @@ const NavOptions = () => {
                 </View>
                 <Text style={tw `mt-2 text-lg font-semibold`}>{item.title}</Text>
                 <Icon
-                    type='antIcon'
+                    style={tw `p-2 bg-black rounded-full w-10 mt-4`}
+                    name='arrowright'
+                    color='white'
+                    type='antdesign'
                 />
             </TouchableOpacity>
         )} 
